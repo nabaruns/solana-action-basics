2e908a205d5dd7fdd599e04af9cb2a98654de434
nabaruns <nabsarkar@gmail.com>
2024-09-02 00:15:08 +0400

    first commit

---
+++ src/app/api/actions/memo/route.ts
@@ -0,0 +1,63 @@
+import { ActionGetResponse, ActionPostRequest, ActionPostResponse, ACTIONS_CORS_HEADERS, createPostResponse, MEMO_PROGRAM_ID } from "@solana/actions"
+import { clusterApiUrl, ComputeBudgetProgram, Connection, PublicKey, Transaction, TransactionInstruction } from "@solana/web3.js";
+import cluster from "cluster";
+
+export const GET = (req: Request) => {
+    const payload: ActionGetResponse = {
+        icon: new URL("/solana_devs.jpg", new URL(req.url).origin).toString(),
+        label: "Get Memo",
+        description: "This is super simple action",
+        title: "Memo demo",
+    };
+
+    return Response.json(payload, {
+        headers: ACTIONS_CORS_HEADERS
+    });
+}
+
+export const OPTIONS = GET;
+
+export const POST = async (req: Request) => {
+    try {
+        const body: ActionPostRequest = await req.json();
+
+        let account: PublicKey;
+        try {
+            account = new PublicKey(body.account);
+        } catch (error) {
+            return Response.json("Invalid account", { status: 400, headers: ACTIONS_CORS_HEADERS });
+        }
+
+        const transaction = new Transaction();
+
+        transaction.add(
+            // note: `createPostResponse` requires at least 1 non-memo instruction
+            ComputeBudgetProgram.setComputeUnitPrice({
+                microLamports: 1000,
+            }),
+            new TransactionInstruction({
+                programId: new PublicKey(MEMO_PROGRAM_ID),
+                data: Buffer.from("This is a test memo", "utf-8"),
+                keys: [],
+            })
+        )
+
+        transaction.feePayer = account;
+
+        const connection = new Connection(clusterApiUrl("devnet"));
+        transaction.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
+
+        const payload: ActionPostResponse = await createPostResponse({
+            fields: {
+                transaction,
+            },
+            // signers: [],
+        });
+
+        return Response.json(payload, {
+            headers: ACTIONS_CORS_HEADERS
+        });
+    } catch (error) {
+        return Response.json("An unknown error occurred", { status: 400 });
+    }
+}
